1adbeb9e9ab78966654e3fc362617868f37a3462
Itsusinn奕䜣 <itsusinn@foxmail.com>
2021-02-15 14:15:17 +0800

    Initial commit

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,56 +1,55 @@
-#[derive(Debug)]
-struct Rectangle {
-    width:i32, height:i32
-}
-#[derive(Debug)]
-struct Color(f32,f32,f32);
+extern crate sdl2;
+extern crate gl;
+
+use sdl2::{
+    pixels::{Color},
+    event::{Event},
+    keyboard::{Keycode}
+};
+use std::time::Duration;
+
 fn main() {
-    let rect = Rectangle {width:23,height:23};
-    let color = Color(2.0,2.0,2.0);
-    println!("rect: {:?}", rect);
-    println!("color: {:?}", color);
-    enum Book {
-        Papery(i32),
-        Electronic { url:String }
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4,5);
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
+        .opengl()
+        .resizable()
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    unsafe {
+        gl::Viewport(0,0,900,700);
+        gl::ClearColor(0.3,0.3,0.5,1.0);
+    }
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT)
+        }
+        window.gl_swap_window()
     }
 }
+fn shader_from_sources(
+    sources:&str
+) -> Result<gl::types::GLuint,String> {
 
-// extern crate sdl2;
-//
-// use sdl2::pixels::Color;
-// use sdl2::event::Event;
-// use sdl2::keyboard::Keycode;
-// use std::time::Duration;
-//
-// fn main() {
-//     let sdl_context = sdl2::init().unwrap();
-//     let video_subsystem = sdl_context.video().unwrap();
-//     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-//         .position_centered()
-//         .build()
-//         .unwrap();
-//     let mut canvas = window.into_canvas().build().unwrap();
-//     canvas.set_draw_color(Color::RGB(0, 255, 255));
-//     canvas.clear();
-//     canvas.present();
-//     let mut event_pump = sdl_context.event_pump().unwrap();
-//     let mut i = 0;
-//     'running: loop {
-//         i = (i + 1) % 255;
-//         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-//         canvas.clear();
-//         for event in event_pump.poll_iter() {
-//             match event {
-//                 Event::Quit {..} |
-//                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-//                     break 'running
-//                 },
-//                 _ => {}
-//             }
-//         }
-//         // The rest of the game loop goes here...
-//
-//         canvas.present();
-//         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-//     }
-// }
+}
